af95a045faade6cf366d780a774640cc92680c24
Orlando Martinez <orjamar@gmail.com>
2023-03-26 20:00:52 -0500

    Completed JavaScript Advanced Functions: First-Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (p) { return p.slice(0,2);};
+const returnLastTwoDrivers = function (p) { return p.slice(-2);};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(i){
+    return function(fare) {
+       return i * fare;
+    }
+}
+
+function fareDoubler(fare){
+    const fd = createFareMultiplier(2);
+    return fd(fare);
+}
+
+function fareTripler(fare){
+    const fd = createFareMultiplier(3);
+    return fd(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, f){
+  if (f===returnFirstTwoDrivers){
+    return arrayOfDrivers.slice(0,2);
+  }
+  else {
+    return arrayOfDrivers.slice(-2);
+  }
+}
